151fa7b8e1bf6db36e261dc62e25c456fa5623ae
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-04-19 13:56:57 -0300

    Add a function to write the hardware list into a file

--- Program.cs
+++ Program.cs
@@ -1,71 +1,82 @@
-﻿using System.Management;
+﻿#pragma warning disable CA1416 // Validate platform compatibility
 
-double ByteToGB(double value)
+using System.Management;
+
+List<string> output = [GetGeneralInfo(), GetCPUInfo(), GetVideoInfo(), GetRAMInfo()];
+
+File.WriteAllLines("./file/log.txt", output);
+
+static double ByteToGB(double value)
 {
     return Math.Round(value / 1024d / 1024d / 1024d, 2);
 }
 
-GetGeneralInfo();
-GetRAMInfo();
-GetVideoInfo();
-GetCPUInfo();
-
-void GetGeneralInfo()
+static string GetGeneralInfo()
 {
+    string temp = "\n";
     using (ManagementObjectSearcher searcher = new("SELECT * FROM Win32_ComputerSystem"))
     {
+        temp += $"###### General Section ######\n\n";
         foreach (var mo in searcher.Get())
         {
-            Console.WriteLine($"Name: {mo["Name"]}");
-            Console.WriteLine($"Manufacturer: {mo["Manufacturer"]}");
-            Console.WriteLine($"Model: {mo["Model"]}");
-            Console.WriteLine($"Total physical memory: {ByteToGB((ulong)mo["TotalPhysicalMemory"])} GB");
-            Console.WriteLine($"Number of processors: {mo["NumberOfProcessors"]}");
-            Console.WriteLine();
+            temp += $"Name: {mo["Name"]}\n";
+            temp += $"Manufacturer: {mo["Manufacturer"]}\n";
+            temp += $"Model: {mo["Model"]}\n";
+            temp += $"Total physical memory: {ByteToGB((ulong)mo["TotalPhysicalMemory"])} GB\n";
+            temp += $"Number of processors: {mo["NumberOfProcessors"]}\n";
+            temp += "\n";
         }
     }
+    return temp;
 }
 
-void GetRAMInfo()
+static string GetRAMInfo()
 {
+    string temp = "\n";
     using (ManagementObjectSearcher searcher = new("SELECT * FROM Win32_PhysicalMemory"))
     {
-
+        temp += $"###### RAM Section ######\n\n";
         foreach (var mo in searcher.Get())
         {
-            Console.WriteLine($"RAM: {ByteToGB((ulong)mo["Capacity"])} GB");
-            Console.WriteLine($"Type: {mo["MemoryType"]}");
-            Console.WriteLine($"Speed: {mo["Speed"]} MHz");
-            Console.WriteLine();
+            temp += $"RAM: {ByteToGB((ulong)mo["Capacity"])} GB\n";
+            temp += $"Type: {mo["MemoryType"]}\n";
+            temp += $"Speed: {mo["Speed"]} MHz\n"; 
+            temp += "\n";
         }
     }
+    return temp;
 }
 
-static void GetVideoInfo()
+static string GetVideoInfo()
 {
+    string temp = "\n";
     using (ManagementObjectSearcher searcher = new("SELECT * FROM Win32_VideoController"))
     {
-
+        temp += $"###### Video Section ######\n\n";
         foreach (var mo in searcher.Get())
         {
-            Console.WriteLine($"Graphics card: {mo["Name"]}");
-            Console.WriteLine();
+            temp += $"Graphics card: {mo["Name"]}\n";
+            temp += "\n";
         }
     }
+    return temp;
 }
 
-static void GetCPUInfo()
+static string GetCPUInfo()
 {
+    string temp ="\n";
     using (ManagementObjectSearcher searcher = new("SELECT * FROM Win32_Processor"))
     {
-
+        temp += $"###### General Section ######\n\n";
         foreach (var mo in searcher.Get())
         {
-            Console.WriteLine($"Processor: {mo["Name"]}");
-            Console.WriteLine($"Number of cores: {mo["NumberOfCores"]}");
-            Console.WriteLine($"Number of logical processors: {mo["NumberOfLogicalProcessors"]}");
-            Console.WriteLine($"Max clock speed: {mo["MaxClockSpeed"]} MHz");
-            Console.WriteLine();
+            temp += $"Processor: {mo["Name"]}\n";
+            temp += $"Number of cores: {mo["NumberOfCores"]}\n";
+            temp += $"Number of logical processors: {mo["NumberOfLogicalProcessors"]}\n";
+            temp += $"Max clock speed: {mo["MaxClockSpeed"]} MHz\n";
+            temp += "\n";
         }
     }
+    return temp;
 }
+#pragma warning restore CA1416 // Validate platform compatibility
